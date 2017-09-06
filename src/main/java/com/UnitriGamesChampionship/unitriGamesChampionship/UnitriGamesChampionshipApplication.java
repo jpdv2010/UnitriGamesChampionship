fe2cadf6ce230e197befe182fddbdc6c19ca1b3f
jpdv2010 <joaopaulodv2011@gmail.com>
2017-09-06 12:53:45 -0300

    first commit

---
+++ src/main/java/com/UnitriGamesChampionship/unitriGamesChampionship/UnitriGamesChampionshipApplication.java
@@ -0,0 +1,12 @@
+package com.UnitriGamesChampionship.unitriGamesChampionship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitriGamesChampionshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitriGamesChampionshipApplication.class, args);
+	}
+}
---
+++ src/test/java/com/UnitriGamesChampionship/unitriGamesChampionship/UnitriGamesChampionshipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.UnitriGamesChampionship.unitriGamesChampionship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UnitriGamesChampionshipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
